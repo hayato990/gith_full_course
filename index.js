e50009b93e2d03d9c294e5ce65e2fc05f1aee3f7
John Doe <spiritmatter70@gmail.com>
2025-02-24 10:13:53 +0900

    関数を呼び出した

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 function greet(){
   console.log("Hello from the main branch!");
 }
-=======
-function greet(){
-    console.log("Hello from the local branch");
-}
->>>>>>> b0862ad (私たちの修正)
+
+greet();
