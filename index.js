50d9f9790301ca94294b1c4759f6ccb0c1848381
John Doe <spiritmatter70@gmail.com>
2025-02-24 10:10:03 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function greet(){
   console.log("Hello from the main branch!");
 }
+=======
+function greet(){
+    console.log("Hello from the local branch");
+}
+>>>>>>> b0862ad (私たちの修正)
